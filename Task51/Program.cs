25fe9e8cdd21ee78aa21ba3bad2af5d1fdfafe8a
Kommissar Egro <ugalovnik666@gmil.com>
2023-09-01 19:16:51 +1000

    решение задачи 51

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумернный массив. Найдите сумму элементов, находящихся на главное диагонали
+// с индексами (0,0);(1,1)  и т.д.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int SumMainElemMatrix(int[,] matrix)
+{
+    int sum = 0;
+    // int minSize = matrix.GetLength(0) < matrix.GetLength(1) ? matrix.GetLength(0) : matrix.GetLength(1);
+    // for (int i = 0; i < mineSize; i++)
+        for (int i = 0; i < matrix.GetLength(0) && i < matrix.GetLength(1); i++)
+    {
+        sum += matrix[i, i];
+    }
+
+    return sum;
+
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 1, 10);
+PrintMatrix(array2d);
+
+int result = SumMainElemMatrix(array2d);
+Console.WriteLine($"Сумма элементов, находящихся на главное диагонали = {result}");
